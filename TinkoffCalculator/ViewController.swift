fcbd53d80dc62b1136d9a0659f55f9c2e2d13a55
Myasnikov Vladislav Nikolaevich <v.myasnikov@robo.cash>
2024-02-25 20:29:44 +0300

    [TASK-1] Add buttons UI

--- TinkoffCalculator/ViewController.swift
+++ TinkoffCalculator/ViewController.swift
@@ -9,11 +9,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBAction func buttonPressed(_ sender: UIButton) {
+        guard let buttonText = sender.currentTitle else { return }
+        
+        print(buttonText)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
 }
-
